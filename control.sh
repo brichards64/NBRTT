e62d7884fe014382fee5c57dbd580be704fcb22a
brichards <b.richards@qmul.ac.uk>
2015-11-13 19:12:58 +0000

    addid source line to control

--- control.sh
+++ control.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 cd /data/richards/NBRTT/
-#./build.sh
-#./runsim.sh
+./build.sh
+./runsim.sh
 
 cd Validation
+source ../build/ts-titus/Source_At_Start.sh
 ./newmakewebpage.sh
 cd ..
 
